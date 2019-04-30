85351cee916b6079b092e8b6c220c1fc29863dd0
Andrew <xie.andrew2235@gmail.com>
2019-04-30 13:33:58 -0700

    updated file structure for decoupled app, this will be the react repo

--- db/seed.js
+++
@@ -1,3 +0,0 @@
-db.farm.insert({"firstName": "John", "lastName":"Doe", "email":"jd@gmail.com" , "phone":"123456566","city":"San Francisco", "state":"CA","username": "pie123","password":"123456", "ratingu":0, "items": [], "wishlist":[]})
-
-db.farm.insert({"firstName": "Jane", "lastName":"Doe", "email":"janed@gmail.com" , "phone":"123456567","city":"Oakland", "state":"CA","username": "pumpkin123","password":"1223456", "rating":0, "items": [], "wishlist":[]})
