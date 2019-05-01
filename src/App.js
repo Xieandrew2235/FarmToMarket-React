477e3e00ec1e898ba901c09cf62fb4fa99d3af44
Andrew <xie.andrew2235@gmail.com>
2019-05-01 16:05:20 -0700

    added dashboard and registration page, along with registration form that is redirected to when user hits registration from root page; also cleaned up some small snippets of faulty code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Navbar from './components/Nav'
-import CardRow from './components/CardRow'
+// import CardRow from './components/CardRow'
+import Landing from './pages/landing';
+import Registration from './pages/registration';
+import Dashboard from './pages/dashboard';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <div className="container">
-        <CardRow />
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/registration" component={Registration} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route path="/" component={Landing} />
+        </Switch>
       </div>
-    </div>);
+    </Router>
+
+
+
+      // <div className="container">
+      //    <CardRow />
+      //    <CardRow />
+      //  </div>
+    );
 }
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-function NavBar (){
+function NavBar() {
     return (
         <nav>
-        <div className="nav-wrapper">
-            <a href="/" className="brand-logo center">Project 3</a>
-            <ul className="right hide-on-med-and-down">
-                <li><a className="waves-effect waves-light btn">Login<i
-                            className="material-icons right">account_circle</i></a></li>
-                <li><a className="waves-effect waves-light btn">Register<i
-                            className="material-icons right">account_circle</i></a></li>
-
-            </ul>
-            <ul id="nav-mobile" className="left hide-on-med-and-down">
-                <li><a href="sass.html">Home</a></li>
-                <li><a href="badges.html">Account</a></li>
-            </ul>
-        </div>
-    </nav>
+            <div className="nav-wrapper">
+                <a href="/" className="brand-logo center">Project 3</a>
+                <ul className="right hide-on-med-and-down">
+                    <li>
+                        <Link to="/login">
+                            Login<i className="material-icons right">account_circle</i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/registration">
+                            Register<i className="material-icons right">account_circle</i>
+                        </Link>
+                    </li>
+                </ul>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                    <li><a href="badges.html">Account</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
