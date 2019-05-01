f0189f4e0f0f8789e86616ed0261388d9a509654
Andrew <xie.andrew2235@gmail.com>
2019-04-30 21:30:41 -0700

    updated navbar and added component CardRow. debugging small component related error tomorrow morning with TAs

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import "./Nav.css";
 
 function NavBar (){
     return (
         <nav>
         <div className="nav-wrapper cyan darken-3">
-            <a href="/" className="brand-logo center">Project 3</a>
+            <a href="/" className="brand-logo center">The Trading Post</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
                 <li><a href="sass.html">Home</a></li>
                 <li><a href="badges.html">Account</a></li>
-                <li><a href="collapsible.html">About Us</a></li>
+                <li><a href="collapsible.html">Wishlist</a></li>
             </ul>
 
             <ul className="right hide-on-med-and-down">
