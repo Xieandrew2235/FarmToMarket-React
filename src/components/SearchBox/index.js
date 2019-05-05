d28ed7797158b9ce61585169d754ab2556080c1f
Andrew <xie.andrew2235@gmail.com>
2019-05-04 22:59:36 -0700

    updated search box with value, and mostly finished the logic for both merchant and farmer dashboard. small errors to debug but code should be mostly on point with what we're looking for

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -8,6 +8,7 @@ return (
   <div class="row">
     <div class="input-field col s12 searchTerm">
       <textarea id="textarea1" class="materialize-textarea"></textarea>
+      <input id="search" type="search" name="searchTerm" value={this.state.searchTerm} onChange={this.handleInputChange} required />
       <label for="textarea1">Search</label>
     </div>
   </div>
--- src/pages/merchantdashboard.js
+++ src/pages/merchantdashboard.js
@@ -1,104 +1,80 @@
 import React, { Component } from 'react';
 import SearchBox from '../components/SearchBox';
 import SearchBtn from '../components/SearchBtn';
-import MerchantRegistration from '../components/Form/MerchantRegistrationForm';
+import MerchantRegistrationForm from '../components/Form/MerchantRegistrationForm';
 import CardRow from '../components/CardRow';
 import API from '../utils/API';
 
-// convert this into a smart component with class MerchantDB extend React.Component
-// add a state with a data array
-// 
-// function MerchantDB () {
-//         return (
-//             <div>
-//                 <div className="container">
-//                     <SearchBox />
-//                     <SearchBtn />
-//                 </div>
-//             </div>
-//         )
-//     }
+class MerchantDB extends Component {
+  state = {
+    name: [],
+    address: "",
+    city: "",
+    state: "",
+    email: "",
+    phone: ""
+  };
 
-    class MerchantDB extends Component {
-        state = {
-          name: [],
-          address: "",
-          city: "",
-          state: "",
-          email: "",
-          phone: ""
-        };
-        handleInputChange = event => {
-          const { name, value } = event.target;
-          this.setState({
-            [name]: value
-          });
-        };
-        
-        handleFormSubmit = event => {
-          event.preventDefault();
-        
-          API.getFarmersByProduce(this.state.prod_Name)
-            .then(res => {
-                console.log(res);
-                this.setState({
-                    farmerData: res.dataValues
-                });
-            });
-        };
-        
-        componentDidMount() {
-          this.loadPeople();
-        }
-        
-        loadPeople = () => {
-          API.getRecentPeople()
-            .then(
-              res => { 
-                this.setState({ returnedPeople: res.data, isLoaded: true }) 
-                // console.log(res.data)
-              },
-              error => {
-                this.setState({ isLoaded: true, error });
-              }
-            )
-          //.catch(err => console.log(err));
-        };
-        
-        render(){
-          return (
-            <div>
-            
-        
-             {/* <form className="form"> */}
-                  {/* <MerchantRegistration></MerchantRegistration> */}
-        
-                   <SearchBox
-                     value={this.state.searchTerm}
-                     onChange={this.handleInputChange}
-                     name="searchTerm"
-                     placeholder="Search for farmers!"
-                   />
-                   <SearchBtn
-                     disabled={!(this.state.searchTerm)}
-                     onClick={this.handleFormSubmit}
-                   /> 
-                    
-                     
-                 {/* </form> */}
-             {/*  <SearchBar></SearchBar> */}
-             
-            
-              <div className="container">
-              
-                <CardRow 
-                items={this.state.returnedPeople}/>
-        
-             {/*  <Postform></Postform>  */}
-              </div>
-            </div>
-            )
-          }
-          
-        }
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+    API.getFarmersByProduce(this.state.prod_Name)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          farmerData: res.data,
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPeople();
+  }
+
+  loadPeople = () => {
+    API.searchFarmer()
+      .then(
+        res => {
+          this.setState({ returnedPeople: res.data, isLoaded: true })
+        },
+        error => {
+          this.setState({ isLoaded: true, error });
+        })
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+
+          <SearchBox
+            value={this.state.searchTerm}
+            onChange={this.handleInputChange}
+            name="searchTerm"
+            placeholder="Search for farmers!"
+          />
+          <SearchBtn
+            disabled={!(this.state.searchTerm)}
+            onClick={this.handleFormSubmit}
+          />
+
+          <div className="container">
+
+            <CardRow
+              items={this.state.returnedPeople} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export default MerchantDB;
